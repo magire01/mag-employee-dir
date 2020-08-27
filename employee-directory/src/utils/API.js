4a5989cac0d9a3d33d6937d164c169bd47b292bb
Mark Gire <magire01@gmail.com>
2020-08-27 02:34:23 -0400

    Deleted extra folder

--- employee-directory/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const randomURL = "https://randomuser.me/api/?results=10";
-
-export default {
-    getData: function() {
-        return axios.get(randomURL);
-    }
-}
